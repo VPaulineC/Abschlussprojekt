49042fad3b7e96ccc12b631db53ba0575915fb5d
VPaulineC <vp2765@mci4me.at>
2024-06-10 13:51:06 +0200

    update person page

--- person_page.py
+++ person_page.py
@@ -25,7 +25,6 @@ def person_page():
     person_names = read_person_data.get_person_list(read_person_data.load_person_data())
     # Auswahlbox, wenn Personen anzulegen sind
     st.session_state.aktuelle_versuchsperson = st.selectbox('choose a person', options = ["choose"] + person_names, key="sbVersuchsperson")
-    
 
     #-------------
     if st.session_state.aktuelle_versuchsperson in person_names:
@@ -33,12 +32,37 @@ def person_page():
         selected_person = person.Person(person_dict)
         st.session_state.picture_path = selected_person.picture_path
 
-        # Weitere Daten wie Geburtsdatum etc. schön anzeigen
-        st.header("Personendaten:")
-        st.write("ID: ", selected_person.id)
-        st.write("Vorname: ", selected_person.firstname)
-        st.write("Nachname: ", selected_person.lastname)
-        st.write("Geburtsdatum: ", selected_person.date_of_birth)
-        st.write("Alter: ", selected_person.age)
-        st.write("Maximale Herzfrequenz: ", selected_person.max_heart_rate)
-        st.write("EKG-Daten: ", selected_person.ekg_data)
+        col1, col2 = st.columns([1, 1])
+
+        with col2:
+        # show the person Data
+            st.header("Personendaten:")
+            #st.write("ID: ", selected_person.id)
+            st.write("Vorname: ", selected_person.firstname)
+            st.write("Nachname: ", selected_person.lastname)
+            st.write("Geburtsdatum: ", selected_person.date_of_birth)
+            st.write("Alter: ", selected_person.age)
+            st.write("Maximale Herzfrequenz: ", selected_person.max_heart_rate)
+            #st.write("EKG-Daten: ", selected_person.ekg_data)
+
+        with col1:
+        # show the image
+            image = Image.open(st.session_state.picture_path)
+            st.image(image, caption=st.session_state.aktuelle_versuchsperson)
+
+        ekg_ids = [ekg["id"] for ekg in selected_person.ekg_data]
+        # Show ekg in a list
+        selected_id = st.selectbox("choose EKG: ", options= ekg_ids, key="sbEKG")
+
+        # EKG Plot und HR Plot
+        ekg_dict = ekgdata.EKGdata.load_by_id(selected_id)
+        #ekg_dict = EKGdata.load_by_id(id_from_selectionbox)
+        ekg_data = ekgdata.EKGdata(ekg_dict)
+        #------------------------------------------------------------
+        df = ekg_data.get_df()
+        #print(df.head())
+        peaks = ekgdata.EKGdata.find_peaks(df['EKG in mV'], 340, 5)
+        #------------------------------------------------------------
+        fig = ekgdata.EKGdata.plot_ekg(df, peaks)
+        #fig.show()
+        st.plotly_chart(fig)
